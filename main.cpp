7590b4f99762bd32f1ad228c98b0c70d3830c2eb
AmirN347 <amirn2203@gmail.com>
2024-07-06 10:23:17 -0400

    Practiced opening and dealing with the binary files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+
 
 using std::cout, std::endl;
 int main() {
-    cout<<"This is the beginning of the neural network project" <<endl;
+    //opening a binary file
+    std::ifstream training_data;
+    training_data.open("t10k-images.idx3-ubyte",std::ios::binary | std::ios::in);
+    if(!training_data.is_open()){
+        cout<<"File couldn't open"<<endl;
+    }
+    else{
+        cout<<"The file opened"<<endl;
+        u_int64_t y;
+        cout<<sizeof(y)<<endl;
+        training_data.read(reinterpret_cast<char*>(&y),sizeof(y));
+        cout<<y<<endl;
+    }
+
     return 0;
 }
